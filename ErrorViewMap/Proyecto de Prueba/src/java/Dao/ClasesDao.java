c7285880cd47ed62cfc32ba1471de07343e24484
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-04-22 16:57:33 -0500

    Elimina código que no se usa.

--- ErrorViewMap/Proyecto de Prueba/src/java/Dao/ClasesDao.java
+++ ErrorViewMap/Proyecto de Prueba/src/java/Dao/ClasesDao.java
@@ -5,10 +5,10 @@
  */
 package Dao;
 
+import org.hibernate.Session;
+
 import logica.HibernateUtil;
 import modelo.Alumno;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
 
 /**
  *
@@ -16,40 +16,17 @@ import org.hibernate.Session;
  */
 public class ClasesDao {
 
-    private int idAlumno;
-    private String ANombre;
-    private String AContrasenha;
-    private String ACorreo;
 
-    public void indroducirAlumno(int idAlumno, String nombre, String contrasena, String corro) {
+    public void indroducirAlumno(int idAlumno, String nombre, String contrasena, String correo) {
 
         Session session = confHibenate.getSessionFactory().getCurrentSession();
-        
+
         Alumno n = new Alumno();
         n.setIdAlumno(idAlumno);
-        n.setANombre(ANombre);
-        n.setAContrasenha(AContrasenha);
-        n.setACorreo(ACorreo);
-        
-        try {
-            session.beginTransaction();
-            session.save(n);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            System.out.println("error del dao");
-            session.getTransaction().rollback();
-        }
-    }
-    public void indroducirAlumno( String nombre, String contrasena, String corro) {
+        n.setANombre(nombre);
+        n.setAContrasenha(contrasena);
+        n.setACorreo(correo);
 
-        Session session = HibernateUtil.getSessionFactory().openSession(); 
-        
-        Alumno n = new Alumno();
-        
-        n.setANombre(ANombre);
-        n.setAContrasenha(AContrasenha);
-        n.setACorreo(ACorreo);
-        
         try {
             session.beginTransaction();
             session.save(n);
@@ -59,6 +36,25 @@ public class ClasesDao {
             session.getTransaction().rollback();
         }
     }
-    
+
+    public void indroducirAlumno(String nombre, String contrasena, String correo) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Alumno n = new Alumno();
+
+        n.setANombre(nombre);
+        n.setAContrasenha(contrasena);
+        n.setACorreo(correo);
+
+        try {
+            session.beginTransaction();
+            session.save(n);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("error del dao");
+            session.getTransaction().rollback();
+        }
+    }
 
 }
